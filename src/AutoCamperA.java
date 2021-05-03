a28f13066cc440de98dfc692f6915498af19860d
Khorp <rasm523c@easv365.dk>
2021-05-03 10:48:28 +0200

    New stuff 3

--- src/AutoCamperA.java
+++ src/AutoCamperA.java
@@ -1,7 +1,24 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+
 public class AutoCamperA implements AutoCamper{
 
+    private ObservableList<ImageView> campers = FXCollections.observableArrayList();
+    //private File file = new File(getClass().getResourceAsStream("AutoCampers\\1.jpg"));
+    private Image[] images = {new Image(getClass().getResourceAsStream("AutoCampers\\1.jpg")),
+            new Image(getClass().getResourceAsStream("AutoCampers\\2.jpg"))};
 
+    public ObservableList getCampers() {
+        return campers;
+    }
 
+    public AutoCamperA(){
+        campers.addAll(new ImageView(images[0]),new ImageView(images[1]));
+    }
 
     @Override
     public void setCamperType() {
--- src/Controller.java
+++ src/Controller.java
@@ -2,7 +2,14 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
 
@@ -12,8 +19,20 @@ public class Controller {
     @FXML
     TextField bookingWeek;
 
-    public void initialize(){
+    @FXML
+    ListView<ImageView> ListView;
+    AutoCamperA camperA = new AutoCamperA();
+    List<Booking> bookingList = new ArrayList<>();
 
+    public void initialize(){
+        ListView.setItems(camperA.getCampers());
+        createBooking.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                bookingList.add(new Booking(bookingWeek.getText(),1));
+                bookingWeek.setText("done");
+            }
+        });
     }
 
 
