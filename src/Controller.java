9d31090b02ed95dd87b9a00e31b9e6d1ba038b05
Khorp <rasm523c@easv365.dk>
2021-05-03 10:10:10 +0200

    New stuff 2

---
+++ src/Controller.java
@@ -0,0 +1,20 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    Button createBooking;
+
+    @FXML
+    TextField bookingWeek;
+
+    public void initialize(){
+
+    }
+
+
+}
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,22 +1,54 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainApp extends Application {
 
-
+    Button createBooking;
+    Label autoCamperText;
+    TextField bookingWeeks,lol;
+    List<Booking> bookingList =new ArrayList<>();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Pane pane = new Pane();
+        Parent root = FXMLLoader.load(getClass().getResource("stuff.fxml"));
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+        /*
+        VBox pane = new VBox();
         Scene scene = new Scene(pane,500,500);
 
+        createBooking = new Button("Create Booking");
+        autoCamperText = new Label("AutoCamper");
+        bookingWeeks = new TextField();
+        bookingWeeks.setPromptText("WeekStart-WeekEnd");
+        lol = new TextField();
 
+        createBooking.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                bookingList.add(new Booking(bookingWeeks.getText(),1));
+            }
+        });
 
-        pane.getChildren().addAll();
+        pane.getChildren().addAll(autoCamperText,bookingWeeks,createBooking,lol);
         primaryStage.setTitle("Program");
         primaryStage.setScene(scene);
         primaryStage.show();
+         */
     }
 }
