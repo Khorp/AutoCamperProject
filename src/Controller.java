8f0ad975a3438ecbaccec881d912059c79474c84
Khorp <rasm523c@easv365.dk>
2021-05-05 09:01:17 +0200

    Create booking upgraded

--- src/Controller.java
+++ src/Controller.java
@@ -3,6 +3,9 @@ import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -11,7 +14,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,25 +39,39 @@ public class Controller {
 
     public void initialize(){
         ListView.setItems(camperA.getCampers());
-        createBooking.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                if (Integer.parseInt(bookingWeek.getText())>=0&&Integer.parseInt(bookingWeek.getText())<=52&&
-                        Integer.parseInt(bookingWeek2.getText())>=0&&Integer.parseInt(bookingWeek2.getText())<=52) {
-                    bookingList.add(new Booking(bookingWeek.getText() + "-" + bookingWeek2.getText(), 1));
-                    pickedAuto.setText("done");
-                }
-                else {
-                    pickedAuto.setText("Invalid week number");
-                }
-            }
-        });
     }
 
     public void autoClicked(){
         //need to get text from label from a vbox witch is inside a listview
         String labelText = ListView.getSelectionModel().getSelectedItem().getChildren().get(0).toString();
         pickedAuto.setText(labelText);
+    }
+
+    public void createBooking(){
+
+        if (Integer.parseInt(bookingWeek.getText())>=0&&Integer.parseInt(bookingWeek.getText())<=52&&
+                Integer.parseInt(bookingWeek2.getText())>=0&&Integer.parseInt(bookingWeek2.getText())<=52) {
+            bookingList.add(new Booking(bookingWeek.getText() + "-" + bookingWeek2.getText(), 1));
+            pickedAuto.setText("done");
+            try {
+                handleCreatePlaylistMenu();
+            }catch (Exception e){}
+        }
+        else {
+            pickedAuto.setText("Invalid week number");
+        }
+    }
+
+    public void handleCreatePlaylistMenu() throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("PaymentFXML.fxml"));
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+
+        stage.setTitle("Playlist creator");
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
